dfc21df813506266f7f1ea3af02c6be9312bffa1
redondo <riad.reda.fethi@gmail.com>
2025-09-20 14:09:46 +0200

    Mise à jour Reservation : app.js et README

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import reservationRoutes from './routes/reservation.routes.js';
 const app = express();
 
 // Middleware pour parser le JSON
 app.use(express.json());
+
+
+
+app.use('/api/reservations', reservationRoutes);
+
 
 
 export default app;
