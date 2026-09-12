e41368aafa56246732af5e8500743a622e26e4de
redondo <riad.reda.fethi@gmail.com>
2026-09-12 14:03:53 +0200

    ajout localhost:8080 pour ci cypress et docker

--- backend/src/app.js
+++ backend/src/app.js
@@ -64,7 +64,8 @@ const allowedOrigins = [
   'http://localhost:4200',
   'http://localhost:3000',
   'http://127.0.0.1:4200',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'http://localhost:8080',//CI Docker + Cypress
 ];
 
 app.use(cors({
